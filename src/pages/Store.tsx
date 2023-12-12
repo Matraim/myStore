87e0656d58c8825b54d415aec036656cbc9fcede
Bismillah <cupertino@Matraims-MacBook-Pro.local>
2023-12-12 15:03:48 +0600

    new info

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,11 +1,56 @@
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import StoreImg1 from '/public/mac/Apple-MacBook-Pro-with-Retina-Display-2020-Space-Gray.png';
+import StoreImg2 from '/public/mac/Apple-MacBook-Pro-with-Retina-Display-2020-Space-Gray.png';
+import StoreVideo from '../assets/video/bannerVideo.mp4';
+
 const Store = () => {
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
-    <>
-      <h1 style={{ textAlign: 'center' }}>Store</h1>
-      <p style={{ textAlign: 'center' }}>
-        We are a fantastic company doing amazing Whatch.
-      </p>
-    </>
+    <div style={{ textAlign: 'center', maxWidth: '800px', margin: 'auto' }}>
+      <h1>Store</h1>
+      <p>We are a fantastic company doing amazing things.</p>
+
+      <Slider {...sliderSettings}>
+        <div>
+          <img
+            src={StoreImg1}
+            alt="Description of the image"
+            style={{
+              maxWidth: '100%',
+              height: 'auto',
+              display: 'block',
+              margin: 'auto',
+            }}
+          />
+        </div>
+        <div>
+          <img
+            src={StoreImg2}
+            alt="Description of the image"
+            style={{
+              maxWidth: '100%',
+              height: 'auto',
+              display: 'block',
+              margin: 'auto',
+            }}
+          />
+        </div>
+      </Slider>
+
+      <video width="100%" controls style={{ marginTop: '20px' }}>
+        <source src={StoreVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
   );
 };
 
