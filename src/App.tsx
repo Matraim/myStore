edc9310acfc086afea2b33fff132b1470a16284c
Bismillah <cupertino@Matraims-MacBook-Pro.local>
2023-12-13 00:48:51 +0600

    new new

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,6 @@ import IPhone from './pages/IPhone';
 import Whatch from './pages/Whatch';
 import AirPods from './pages/AirPods';
 import Accessories from './pages/Accessories';
-import LoginForm from './layout/LoginForm';
-import SignUp from './layout/SignUp';
 
 function App() {
   return (
@@ -20,7 +18,7 @@ function App() {
       <Navbar />
       <Container className="mb-4">
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/store" element={<Store />} />
           <Route path="/mac" element={<Mac />} />
           <Route path="/IPad" element={<IPad />} />
@@ -29,8 +27,6 @@ function App() {
           <Route path="/AirPods" element={<AirPods />} />
           <Route path="/Accessories" element={<Accessories />} />
           <Route path="/about" element={<About />} />
-          <Route path="/" element={<LoginForm />} />
-          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Container>
     </ShoppingCartProvider>
