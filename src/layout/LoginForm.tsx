95729960a7b23a5a04e77e638f438d37fdd9b772
Bismillah <cupertino@Matraims-MacBook-Pro.local>
2023-12-16 16:40:17 +0600

    create interface for login form

--- src/layout/LoginForm.tsx
+++ src/layout/LoginForm.tsx
@@ -1,4 +1,10 @@
-const LoginForm = () => {
+import { FC } from 'react';
+
+interface ILoginForm {
+  onSubmit: (values: { email: string; password: string }) => void;
+}
+
+const LoginForm: FC<ILoginForm> = () => {
   return <div>LoginForm</div>;
 };
 
