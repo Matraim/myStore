9e46f7e8d37fbe3c85ef29edbae5b7c83bf651f4
Bismillah <cupertino@Matraims-MacBook-Pro.local>
2023-12-23 17:06:45 +0600

    catalog

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,6 @@
 const About = () => {
   return (
-    <div>
+    <div style={{ textAlign: 'center' }}>
       <h1>About Us Team Matraim</h1>
       <p>We are a fantastic company doing amazing things.</p>
     </div>
